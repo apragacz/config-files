e5a683fa6e9300dde595f676d7660f18405664eb
Andrzej Pragacz <apragacz@o2.pl>
2017-05-11 16:27:46 +0200

    updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 pip install --user --upgrade pip
-pip install --user --upgrade six
+pip install --user --upgrade wheel
+pip install --user --upgrade setuptools
 pip install --user jinja2 six
