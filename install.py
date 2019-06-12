77267502110ff22d81ae71bcedd719fb6d1dad36
Andrzej Pragacz <apragacz@o2.pl>
2019-06-12 15:06:36 +0200

    Handle keyboard interrupts

--- install.py
+++ install.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import os
 
 from installhelpers.installation import Installation
@@ -22,23 +23,26 @@ MODULES = (
 
 
 def main():
-    homedir_path = os.environ['HOME']
-    installation = Installation(
-        homedir_path,
-        os.path.join(homedir_path, 'config-files'),
-    )
-    modules_to_configure = []
-    for module in MODULES:
-        if not installation.prepare_with_module(module):
-            continue
-        modules_to_configure.append(module)
+    try:
+        homedir_path = os.environ['HOME']
+        installation = Installation(
+            homedir_path,
+            os.path.join(homedir_path, 'config-files'),
+        )
+        modules_to_configure = []
+        for module in MODULES:
+            if not installation.prepare_with_module(module):
+                continue
+            modules_to_configure.append(module)
 
-    installation.set_up()
+        installation.set_up()
 
-    for module in modules_to_configure:
-        module.configure(installation)
+        for module in modules_to_configure:
+            module.configure(installation)
 
-    installation.tear_down()
+        installation.tear_down()
+    except KeyboardInterrupt:
+        print("\nInterrupted by user, quitting!")
 
 
 if __name__ == '__main__':
