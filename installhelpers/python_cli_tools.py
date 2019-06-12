5d8afd6acd7f1ca9f4368d7f6a136379402ea026
Andrzej Pragacz <apragacz@o2.pl>
2019-06-12 15:01:33 +0200

    Using pipsi from master as a workaround

--- installhelpers/python_cli_tools.py
+++ installhelpers/python_cli_tools.py
@@ -7,9 +7,13 @@ from .base import print_info, print_step
 def configure(installation):
     print_step('Installing Python CLI tools')
     print_info('Installing pipsi')
-    os.system('curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python')  # noqa: E501
+    os.system('curl -O https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py')  # noqa: E501
+    os.system('python get-pipsi.py --src=git+https://github.com/mitsuhiko/pipsi.git')  # noqa: E501
+    os.system('rm get-pipsi.py')
     reqs_path = os.path.join(
         installation.repo_path, 'dependencies', 'pipsi', 'base.txt')
+    pipsi_path = os.path.join(
+        installation.home_path, '.local', 'bin', 'pipsi')
     with open(reqs_path, 'rt') as f:
         for line in f:
             line = line.strip()
@@ -20,5 +24,6 @@ def configure(installation):
             package_name = line
             print_info('Installing {package_name} via pipsi'.format(
                 package_name=package_name))
-            os.system('pipsi install {package_name}'.format(
+            os.system('{pipsi_path} install {package_name}'.format(
+                pipsi_path=pipsi_path,
                 package_name=package_name))
